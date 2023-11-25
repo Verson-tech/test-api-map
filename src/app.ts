ca795cbb9492f0a6d77bcd44829aa5f954461ce6
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2023-11-25 17:18:18 -0500

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "";
+function  searchAddressHandler(event: Event){
+    event.preventDefault();
+const enteredAddress = addressInput.value;
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}=${GOOGLE_API_KEY}`)
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+
+form.addEventListener('submit', searchAddressHandler);
