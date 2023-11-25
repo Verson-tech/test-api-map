1f53bd1dd540b489dfdacbf1caca791d65fc3731
Vira Murray <vira.avramenko@gmail.com>
2023-11-25 17:41:31 -0500

    test api key

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-const form = document.querySelector('form')!;
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "";
-function  searchAddressHandler(event: Event){
-    event.preventDefault();
-const enteredAddress = addressInput.value;
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
 
-axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}=${GOOGLE_API_KEY}`)
-    .then(response => {
-        console.log(response);
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response);
     })
-    .catch(err => {
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
 }
 
-
-form.addEventListener('submit', searchAddressHandler);
+form.addEventListener("submit", searchAddressHandler);
